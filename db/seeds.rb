a3f8f73854f200d844990855ccd06a2e7566de66
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-10 14:26:49 -0700

    Task A

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.delete_all
+#...
+Product.create!(title: 'Rails Scales!',
+    description:
+        %(<p>
+          <em>Rails doesn’t scale So say the naysayers</em> Theyre wrong. Ruby on Rails runs some of the biggest sites in the world, impacting the lives of millions of users while efficiently crunching petabytes of data. This book reveals how they do it, and how you can apply the same techniques to your applications. Optimize everything necessary to make an application function at scale: monitoring, product design, Ruby code, software architecture, database access, caching, and more. Even if your app may never have millions of users, you reduce the costs of hosting and maintaining it.
+            </p>),
+        image_url: 'scales.jpg',
+        price: 30.95)
+#...
