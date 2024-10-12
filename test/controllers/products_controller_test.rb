f8b3d045ceeb4982e0b9f6e87981783a308f7b1f
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-11 18:41:25 -0700

    Task D

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -40,10 +40,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy product" do
-    assert_difference("Product.count", -1) do
-      delete product_url(@product)
-    end
-
-    assert_redirected_to products_url
+    assert_raises ActiveRecord::RecordNotDestroyed do
+      delete product_url(products(:two))
   end
+  assert Product.exists? (products(:two).id)
 end
