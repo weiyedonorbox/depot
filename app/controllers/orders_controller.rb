6ae69f2a509025de60de45e23f8d7d9d95b0d76e
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-15 16:17:56 -0700

    Task G2 done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -65,9 +65,9 @@ class OrdersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    #def set_order
-     # @order = Order.find(params[:id])
-    #end
+    def set_order
+      @order = Order.find(params[:id])
+    end
 
     # Only allow a list of trusted parameters through.
     def order_params
