3d6c42e8b88364784e45d9993d9e4f3156b88bd3
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-15 16:27:44 -0700

    Task G done

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -5,6 +5,35 @@ class OrdersTest < ApplicationSystemTestCase
     @order = orders(:one)
   end
 
+  test "check dynamic fields" do
+    visit store_index_url
+    click_on "Add to Cart", match: :first 
+    click_on "Checkout"
+    assert has_no_field? "Routing number" 
+    assert has_no_field? "Account number"
+    assert has_no_field? "Credit card number" 
+    assert has_no_field? "Expiration date" 
+    assert has_no_field? "Po number"
+    select "Check", from: "Pay type"
+    assert has_field? "Routing number"
+    assert has_field? "Account number"
+    assert has_no_field? "Credit card number" 
+    assert has_no_field? "Expiration date" 
+    assert has_no_field? "Po number"
+    select "Credit card", from: "Pay type"
+    assert has_no_field? "Routing number" 
+    assert has_no_field? "Account number" 
+    assert has_field? "Credit card number" 
+    assert has_field? "Expiration date" 
+    assert has_no_field? "Po number"
+    select "Purchase order", from: "Pay type"
+    assert has_no_field? "Routing number" 
+    assert has_no_field? "Account number" 
+    assert has_no_field? "Credit card number" 
+    assert has_no_field? "Expiration date" 
+    assert has_field? "Po number"
+  end
+
   test "visiting the index" do
     visit orders_url
     assert_selector "h1", text: "Orders"
