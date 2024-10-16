8d0df353564027b84d9da05847269db01f364dbb
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-15 18:47:01 -0700

    Task H1

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,25 @@
+class OrderMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  default from: "Sam Ruby <depot@example.com>"
+  def received(order)
+    @order = order
+    
+
+    mail to: order.email, subjet: "Pragmatic Store Order Confirmation"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped(order)
+    @order=order
+
+    mail to: order.email, subject: "Pragmatic Store Order Shipped"
+  end
+end
