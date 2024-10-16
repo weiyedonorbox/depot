8bb7683b32dcf2c6f476bad539a1227791b2ddf4
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-16 16:09:53 -0700

    Task H finish

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,9 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        #OrderMailer.received(@order).deliver_later
+        #@order.charge!(pay_type_params) #do not do this
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice: "Thank you for your order." }
         format.json { render :show, status: :created, location: @order }
       else
@@ -69,7 +71,8 @@ class OrdersController < ApplicationController
       params.require(:order).permit(:credit_card_number, :expiration_date) 
     elsif order_params[:pay_type] == "Check"
       params.require(:order).permit(:routing_number, :account_number) 
-    elsif order_params[:pay_type] == "Purchase order" params.require(:order).permit(:po_number) 
+    elsif order_params[:pay_type] == "Purchase order" 
+      params.require(:order).permit(:po_number) 
     else
       {}
     end 
--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -42,10 +42,10 @@ class OrdersTest < ApplicationSystemTestCase
   test "should create order" do
     visit orders_url
     click_on "New order"
-
+    fill_in "Name", with: @order.name
     fill_in "Address", with: @order.address
     fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
+    
     fill_in "Pay type", with: @order.pay_type
     click_on "Create Order"
 
@@ -60,8 +60,8 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in "Address", with: @order.address
     fill_in "Email", with: @order.email
     fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Update Order"
+    select @order.pay_type, from: "order_pay_type" 
+    click_on "Place Order"
 
     assert_text "Order was successfully updated"
     click_on "Back"
@@ -72,5 +72,33 @@ class OrdersTest < ApplicationSystemTestCase
     click_on "Destroy this order", match: :first
 
     assert_text "Order was successfully destroyed"
+  end
+
+  test "check order and delivery" do LineItem.delete_all Order.delete_all
+    visit store_index_url
+    click_on "Add to Cart", match: :first click_on "Checkout"
+    fill_in "Name", with: "Dave Thomas" 
+    fill_in "Address", with: "123 Main Street" 
+    fill_in "Email", with: "dave@example.com"
+    select "Check", from: "Pay type"
+    fill_in "Routing number", with: "123456"
+    fill_in "Account number", with: "987654" 
+    click_button "Place Order"
+    assert_text "Thank you for your order"
+    perform_enqueued_jobs
+    perform_enqueued_jobs
+    assert_performed_jobs 2
+    orders = Order.all 
+    assert_equal 1, orders.size
+    order = orders.first
+    assert_equal "Dave Thomas", order.name
+    assert_equal "123 Main Street", order.address 
+    assert_equal "dave@example.com", order.email 
+    assert_equal "Check", order.pay_type 
+    assert_equal 1, order.line_items.size
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal [ "dave@example.com" ], mail.to
+    assert_equal "Sam Ruby <depot@example.com>", mail [:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
 end
