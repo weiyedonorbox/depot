57cf1075d05f6dca5d18483141883a8475c9af53
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-15 19:02:58 -0700

    Task H2 half done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -64,6 +64,18 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pay_type_params
+    if order_params[:pay_type] == "Credit card"
+      params.require(:order).permit(:credit_card_number, :expiration_date) 
+    elsif order_params[:pay_type] == "Check"
+      params.require(:order).permit(:routing_number, :account_number) 
+    elsif order_params[:pay_type] == "Purchase order" params.require(:order).permit(:po_number) 
+    else
+      {}
+    end 
+  end
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
