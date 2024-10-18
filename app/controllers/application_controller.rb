e8f47a258d52d32ef34ce111fae81535f1411918
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-17 18:31:01 -0700

    Task I3 try to figure out setup()

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  protected
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
+  #allow_browser versions: :modern
 end
