214710ee4cf9b9540c9230e549618901be1960cd
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-30 12:02:23 -0700

    Task J3 es translate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_i18n_locale_from_params
   before_action :authorize
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
@@ -8,5 +9,15 @@ class ApplicationController < ActionController::Base
         redirect_to login_url, notice: "Please log in"
       end
     end
-  #allow_browser versions: :modern
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] =
+            "#{params[:locale]} translation not avaliable"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
 end
