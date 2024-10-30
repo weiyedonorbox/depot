fb57bf35f6145e5e826c64f7ee009e36739edf60
weiyedonorbox <weiye.ding@donorbox.org>
2024-10-30 12:47:29 -0700

    Task J Finished

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,7 +39,7 @@ class OrdersController < ApplicationController
         #OrderMailer.received(@order).deliver_later
         #@order.charge!(pay_type_params) #do not do this
         
-        format.html { redirect_to store_index_url, notice: "Thank you for your order." }
+        format.html { redirect_to store_index_url(locale: I18n.locale), notice: I18n.t(".thanks") }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
