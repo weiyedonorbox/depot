5fc84ac8221d631fc2696c358a3c0f1d6e15d6cb
weiyedonorbox <weiye.ding@donorbox.org>
2024-11-06 15:46:27 -0800

    All function in Task K done except responding

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -2,4 +2,11 @@ class SupportRequestscontroller < ApplicationController
   def index
     @support_requests = SupportRequest.all
   end
+
+  def update
+    support_request = SupportRequest.find(params[:id])
+    support_request.update(response: params.require(:support_request)[:response])
+    SupportRequestMailer.response(support_request).deliver_now
+    redirect_to support_requests_path
+  end
 end
